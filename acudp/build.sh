b3040a79aaa8b5080ec3334614574ebfbc268a47
Chris Pilkington <chris.pilkington@gmail.com>
2024-04-24 23:15:57 +1000

    Use the configurable-host-and-port acudp branch.

--- acudp/build.sh
+++ acudp/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -xe
 
 # Validate the acudp checksum
-ACUDP_VERSION="1.0.0"
+ACUDP_VERSION="configurable-host-and-port"
+#ACUDP_VERSION="1.0.0"
 #ACUDP_SHA256_CHECKSUM="86c21b07fda2d763b2cc70c2f842500fca85ba17516c46274ea54ce466b6838f"
 #echo "$ACUDP_SHA256_CHECKSUM acudp-$ACUDP_VERSION.tar.gz" | sha256sum --check --status
 
@@ -11,7 +12,10 @@ ACUDP_VERSION="1.0.0"
 
 # Clone the branch of acudp that allows a configurable host and port, unfortunately it hasn't been merged in yet
 # https://github.com/vpicon/acudp/pull/1
-git clone --branch configurable-host-and-port --single-branch git@github.com:pilkch/acudp.git "./acudp-$ACUDP_VERSION/"
+#git clone --branch configurable-host-and-port --single-branch git@github.com:pilkch/acudp.git "./acudp-$ACUDP_VERSION/"
+wget "https://github.com/pilkch/acudp/archive/refs/heads/$ACUDP_VERSION.zip"
+mv "$ACUDP_VERSION.zip" "acudp-$ACUDP_VERSION.zip"
+unzip "acudp-$ACUDP_VERSION.zip"
 
 # We want to install to the parent directory
 OUTPUT_DIR=$(realpath ../output)
