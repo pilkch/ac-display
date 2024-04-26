697d0395d14c037bc708493f986264626ca2495d
Chris Pilkington <chris.pilkington@gmail.com>
2024-04-26 13:18:49 +1000

    Run eslint on javascript and json.

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
+
+import js from "@eslint/js";
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+  js.configs.recommended,
+  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'jsonc/rule-name': 'error'
+    }
+  }
+];
